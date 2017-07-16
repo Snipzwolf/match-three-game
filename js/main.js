8667bb90e53bac23402dd71ba47c69accfdb16f7
Snipzwolf <jfcopsey@gmail.com>
2017-07-16 10:44:37 +0100

    made sprite movable

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
   var gem_prefixs = [
     'yellow', 'blue', 'green', 'red', 'purple', 'pink'
   ];
+
   var gem_size = {
-    w: 55
+    w: 55,
     h: 82,
     m: 5
   };
@@ -12,7 +13,9 @@ $(document).ready(function(){
   }
 
   function create() {
-     game.add.sprite(0, 0, 'gems', 'yellow_gem_1');
+     var a = game.add.sprite(0, 0, 'gems', 'yellow_gem_1');
+     a.inputEnabled = true;
+     a.input.enableDrag();
      game.add.sprite(0, 200, 'gems', 'yellow_gem_2');
   }
 
@@ -20,6 +23,5 @@ $(document).ready(function(){
 
   }
 
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-canvas', { preload: preload, create: create, update: update });
-
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-canvas', { preload: preload, create: create, update: update, enableDebug: false});
 });

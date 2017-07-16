90ed5c833caf7975674d56e856c598363661f450
Snipzwolf <jfcopsey@gmail.com>
2017-07-16 16:37:20 +0100

    tiled gems across canvas made canvas size dynamic and fitted to gem size

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  var grid = [15, 9];
+
   var gem_prefixs = [
     'yellow', 'blue', 'green', 'red', 'purple', 'pink'
   ];
@@ -8,20 +10,36 @@ $(document).ready(function(){
     h: 82,
     m: 5
   };
+
   function preload() {
     game.load.atlasJSONHash('gems', 'images/gems.png', 'images/gems-hash.json');
   }
 
   function create() {
-     var a = game.add.sprite(0, 0, 'gems', 'yellow_gem_1');
-     a.inputEnabled = true;
-     a.input.enableDrag();
-     game.add.sprite(0, 200, 'gems', 'yellow_gem_2');
+    var x = 1, y;
+    do{
+      y = 1;
+      do{
+        var xPos = (x * gem_size.w) - gem_size.w,
+            yPos = (y * gem_size.h) - gem_size.h,
+            gemName = gem_prefixs[ game.rnd.integerInRange(0, gem_prefixs.length-1) ] + '_gem_1',
+            gemSprite = game.add.sprite(xPos, yPos, 'gems', gemName);
+
+          console.log({
+            x: x,
+            y: y,
+            xPos: xPos,
+            yPos: yPos,
+            gemName: gemName,
+            gemSprite: gemSprite
+          });
+      }while(y++ < grid[1]);
+    }while(x++ < grid[0]);
   }
 
   function update() {
 
   }
 
-  var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-canvas', { preload: preload, create: create, update: update, enableDebug: false});
+  var game = new Phaser.Game(grid[0] * gem_size.w, grid[1] * gem_size.h, Phaser.AUTO, 'game-canvas', { preload: preload, create: create, update: update, enableDebug: false});
 });
